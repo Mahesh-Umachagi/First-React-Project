1e010fc8ccb0d3a57f7b7b8d450f676a7e804bb8
Mahesh-Umachagi <maheshmumachagi@gmail.com>
2020-04-18 12:43:14 +0530

    composing components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+// import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import Counter from "./components/counter";
+import Counters from "./components/counters";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
+ReactDOM.render( < Counters / > ,
   document.getElementById("root")
 );
 
